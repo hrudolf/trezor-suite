e689036aa8371f575f5feef0c1cd48cec5eeb58f
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-07-20 11:06:52 +0200

    feat(connect-explorer-extension): initial working explorer in extension

---
+++ packages/connect-explorer-extension/src/pages/connect-explorer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+// import { ConnectExplorer } from './ConnectExplorer';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import App from '@trezor/connect-explorer/src/router';
+
+const container = document.getElementById('connect-explorer-container');
+const root = createRoot(container!);
+root.render(<App />);
--- packages/connect-explorer-extension/webpack/prod.webpack.config.ts
+++ packages/connect-explorer-extension/webpack/prod.webpack.config.ts
@@ -11,6 +11,14 @@ const config: webpack.Configuration = {
     entry: {
         popup: path.join(__dirname, '..', 'src', 'pages', 'popup', 'index.tsx'),
         connectManager: path.join(__dirname, '..', 'src', 'pages', 'connect-manager', 'index.tsx'),
+        connectExplorer: path.join(
+            __dirname,
+            '..',
+            'src',
+            'pages',
+            'connect-explorer',
+            'index.tsx',
+        ),
         serviceWorker: path.join(__dirname, '..', 'src', 'background', 'serviceWorker.ts'),
     },
     output: {
@@ -41,12 +49,22 @@ const config: webpack.Configuration = {
                     },
                 },
             },
+            {
+                test: /\.(gif|jpe?g|png|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: './images/[name][contenthash][ext]',
+                },
+            },
         ],
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx'],
         modules: ['node_modules'],
         mainFields: ['browser', 'module', 'main'],
+    },
+    performance: {
+        hints: false,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -60,6 +78,12 @@ const config: webpack.Configuration = {
             chunks: ['connectManager'],
             filename: 'connect-manager.html',
             template: path.join(__dirname, '..', 'src', 'pages', 'connect-manager', 'index.html'),
+            inject: true,
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'connect-explorer.html',
+            template: path.join(__dirname, '..', 'src', 'pages', 'connect-explorer', 'index.html'),
             inject: true,
             minify: false,
         }),
@@ -78,6 +102,10 @@ const config: webpack.Configuration = {
                     to: `${DIST}/vendor`,
                 },
             ],
+        }),
+        new webpack.DefinePlugin({
+            // eslint-disable-next-line no-underscore-dangle
+            'process.env.__TREZOR_CONNECT_SRC': JSON.stringify(process.env.__TREZOR_CONNECT_SRC),
         }),
     ],
 };
